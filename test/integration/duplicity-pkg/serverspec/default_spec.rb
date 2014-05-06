3b7f32a2348e5f08a901feb7763283e8134779fd
Ted Chen <ted@nephilagraphic.com>
2014-05-06 20:57:12 -0300

    Added Ubuntu 14.04

--- test/integration/duplicity-pkg/serverspec/default_spec.rb
+++ test/integration/duplicity-pkg/serverspec/default_spec.rb
@@ -2,8 +2,9 @@ require 'serverspec'
 
 include Serverspec::Helper::Exec
 
-# Ensure duplicity exists
+# Ensure duplicity exists.
+# We're not picky on the actual version.
 describe command('duplicity --version') do
-  it { should return_stdout 'duplicity 0.6.18' }
+  it { should return_stdout /duplicity [0-9]+.[0-9]+.[0-9]+/ }
   it { should return_exit_status 0 }
 end
